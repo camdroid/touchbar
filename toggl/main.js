5d43bade779564ec5b4210c9fb443f5901f5320b
Cam <camherringshaw@gmail.com>
2017-08-31 10:37:35 -0400

    WIP - Trying to get toggl electron app to work

---
+++ toggl/main.js
@@ -0,0 +1,73 @@
+const {app, BrowserWindow, TouchBar} = require('electron')
+const {TouchBarButton, TouchBarLabel, TouchBarSpacer} = TouchBar
+
+const TogglClient = require('toggl-api');
+
+const toggl = new TogglClient({apiToken: ''});
+current_entry = null;
+running = false;
+
+function getCurrEntry() {
+    toggl.getCurrentTimeEntry(function(err, timeEntry) {
+        if(timeEntry) {
+            current_entry = timeEntry;
+            running = true;
+        } else {
+            running = false;
+            current_entry = null;
+        }
+    })
+}
+
+function stopCurrentEntry() {
+    if(current_entry != null) {
+        console.log('Stopping time entry');
+        toggl.stopTimeEntry(current_entry.id, function(err, obj) {
+                if(err != null) {
+                    console.log('couldn\'t stop entry');
+                    console.log(err);
+                } else {
+                    console.log('stopped entry');
+                }
+            });
+    }
+
+
+getCurrEntry()
+const toggl_button = new TouchBarButton({
+    label: 'Toggl!',
+    click: () => {
+        getCurrEntry()
+        console.log('Running: '+running);
+        console.log('Entry');
+        console.log(current_entry);
+        if(current_entry != null) {
+            console.log('Stopping time entry');
+            toggl.stoptimeentry(current_entry.id, function(err, obj) {
+                if(err != null) {
+                    console.log('couldn\'t stop entry');
+                    console.log(err);
+                } else {
+                    console.log('stopped entry');
+                }
+            });
+        }
+    }
+
+})
+
+const touchBar = new TouchBar([toggl_button])
+
+let window
+
+app.once('ready', () => {
+  window = new BrowserWindow({
+    frame: false,
+    titleBarStyle: 'hidden-inset',
+    width: 200,
+    height: 200,
+    backgroundColor: '#000'
+  })
+  window.loadURL('about:blank')
+  window.setTouchBar(touchBar)
+})
